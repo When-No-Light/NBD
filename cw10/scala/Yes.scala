3ec7234ea1141990d0f2e1785b2e48d16facd6d9
Vadym Stakhov <60628579+When-No-Light@users.noreply.github.com>
2021-12-26 04:43:42 +0100

    Delete Yes.scala

--- cw10/scala/Yes.scala
+++
@@ -1,8 +0,0 @@
-class Yes[A](var value: A) extends Maybe[A]  {
-  def applyFunction[R] (fun: A => R) = fun(value)
-  def getOrElse(param: A) = value
-//  def flatMap[R](f: A => Maybe[R]) = f(value)
-//  def map[R](f: A => Maybe[R]) = new Yes(f(value))
-
-
-}
